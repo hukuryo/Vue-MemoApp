7825db36a0f0680fd37004f0b4f3b8595571e125
hukuryo <ryohei.1998.05.09@gmail.com>
2023-04-16 14:38:21 +0900

    一覧画面のレイアウトを調整

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -9,18 +9,40 @@
     <p v-else>メモはありません</p>
   </div>
 </template>
+<style scoped>
+  ul{
+    margin: 0;
+    padding: 0;
+  }
+
+  li{
+    list-style: none;
+    border-bottom: 1px solid#ccc;
+    padding-bottom: 10px;
+    margin-bottom: 10px;
+    text-align: left;
+  }
+
+  li a{
+    color: #999;
+    text-decoration: none;
+    width: 100%;
+    display: block;
+  }
+</style>
 <script>
 export default {
   name: 'HomeView',
   computed: {
     // メモの数が何個あるかを取得する処理
+    // gettersはstoreからメソッドを取得している
     HasMemos() {
-      return this.$store.state.memos.length
+      return this.$store.getters.getCount
     },
     // 他のファイル(今回はstore/index.jsから)データを引っ張ってきて、渡すことができる記述
     memos() {
       // storeからデータを引っ張ってくる処理
-      return this.$store.state.memos
+      return this.$store.getters.getAll
     }
   }
 }
